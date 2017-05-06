4123348c21ba8fe5535281ef24a09a2fa63d3da0
livsmith77 <liv--smith@hotmail.com>
2017-05-06 13:36:14 +0100

    adding tests for comments

--- CA_test_simple.py
+++ CA_test_simple.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from simple import get_commits, read_file
+from CA_simple import get_commits, read_file
 
 class TestCommits(unittest.TestCase):
 
@@ -19,6 +19,19 @@ class TestCommits(unittest.TestCase):
         commits = get_commits(self.data)
         self.assertEqual('Thomas', commits[0]['author'])
         self.assertEqual('r1551925', commits[0]['revision'])
+        self.assertEqual('Renamed folder to the correct name', commits[0]['commit_details'])
 
+    def test_fourth_commit(self):
+        commits = get_commits(self.data)
+        self.assertEqual('Thomas', commits[3]['author'])
+        self.assertEqual('r1551558', commits[3]['revision'])
+        self.assertEqual('Chnaged jira url to htps', commits[3]['commit_details'])
+        
+    def test_ninth_commit(self):
+        commits = get_commits(self.data)
+        self.assertEqual('Vincent', commits[8]['author'])
+        self.assertEqual('r1551347', commits[8]['revision'])
+        self.assertEqual('SFR-108 : removed unnecessary layouts which were copy-pasted from amx. They were enabling help_footer', commits[8]['commit_details'])
+        
 if __name__ == '__main__':
     unittest.main()
