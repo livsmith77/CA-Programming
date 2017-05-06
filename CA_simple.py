1391d4601885c08c03453f6a3d8a91e6c9194eb5
livsmith77 <liv--smith@hotmail.com>
2017-05-06 13:20:30 +0100

    changed number line

--- CA_simple.py
+++ CA_simple.py
@@ -17,7 +17,7 @@ def get_commits(data):
             commit = {'revision': details[0].strip(),
                 'author': details[1].strip(),
                 'date': details[2].strip(),
-                'number_of_lines': details[3].strip().split(' ')[1]
+                'number_of_lines': int(details[3].strip().split(' ')[0])
             }
             # add details to the list of commits.
             commits.append(commit)
@@ -28,13 +28,13 @@ def get_commits(data):
 
 if __name__ == '__main__':
     # open the file - and read all of the lines.
-    changes_file = 'changes_python.log'
+    changes_file = 'changes_python.txt'
     data = read_file(changes_file)
     commits = get_commits(data)
 
     # print the number of lines read
-    print(len(data))
+    # print(len(data))
     #print(commits)
     print(commits[0])
-    print(commits[1]['author'])
-    print(len(commits))
+    #print(commits[1]['author'])
+    #print(len(commits))
